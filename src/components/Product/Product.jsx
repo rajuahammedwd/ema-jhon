d11610501466809f8eebb8116c3d3cdf4f396987
raju720190r <raju720190r@gmail.com>
2023-03-30 22:19:50 +0600

    first commit

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const { img, name, seller, quantity, price, ratings } = props.product;
+    const handleAddToCart =props.handleAddToCart;
+   
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price : ${price}</p>
+            <p>ManuFacturer : {seller}</p>
+            <p>Rating : {ratings} Star</p>
+            </div>
+            <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'>Add To Car <i class="fa-sharp fa-solid fa-cart-shopping"></i></button>
+
+        </div>
+    );
+};
+
+export default Product;
